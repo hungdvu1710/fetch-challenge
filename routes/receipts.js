eb654c4fed1976704512324bbed37ab96b0421a1
Hung Vu <hungvsme@gmail.com>
2023-10-13 12:28:23 -0500

    clean up code

--- routes/receipts.js
+++ routes/receipts.js
@@ -3,32 +3,6 @@ const receipts_router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 let receipts = {};
 
-// receipts examples:
-/**
- * 
-  {
-    "retailer": "Walgreens",
-    "purchaseDate": "2022-01-02",
-    "purchaseTime": "08:13",
-    "total": "2.65",
-    "items": [
-        {"shortDescription": "Pepsi - 12-oz", "price": "1.25"},
-        {"shortDescription": "Dasani", "price": "1.40"}
-    ]
-  }
-
-  {
-    "retailer": "Target",
-    "purchaseDate": "2022-01-02",
-    "purchaseTime": "13:13",
-    "total": "1.25",
-    "items": [
-        {"shortDescription": "Pepsi - 12-oz", "price": "1.25"}
-    ]
-  }
- * 
- */
-
 const calculateReceiptPoints = (receiptId) => {
   const receipt = receipts[receiptId];
   let points = 0;
--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const receipts_router = require('./routes/receipts');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
-})
-
 app.use(express.json());
 
 app.use("/receipts", receipts_router)
